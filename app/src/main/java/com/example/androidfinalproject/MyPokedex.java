2ce0e2cf29d8d14cdb7e4c72b03dce740029bb0e
gabelee <ligabe097@gmail.com>
2024-05-10 04:10:50 -0400

    Items for personal pokedex are added to the list

--- app/src/main/java/com/example/androidfinalproject/MyPokedex.java
+++ app/src/main/java/com/example/androidfinalproject/MyPokedex.java
@@ -12,10 +12,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MyPokedex extends ToolBarSetup {
     List<String> names;
+    ListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,13 +34,37 @@ public class MyPokedex extends ToolBarSetup {
 
         TextView userPokedex = findViewById(R.id.UserPokedexText);
         ListView pokedex = findViewById(R.id.PokedexList);
+        pokedex.setAdapter(adapter = new ListAdapter(this));
 
         SharedPreferences prefs = getSharedPreferences("Name", Context.MODE_PRIVATE);
         String username = prefs.getString("UserName", "Trainer");
         // lol, the string for the preference name and key val was set to all lowercase
         //bug fixed
 
+        DBAdapter db = new DBAdapter(this);
+
         userPokedex.setText(username + "'s Pokedex");
+        List<String> pokemons = db.readTable();
+
+        String uri = "https://pokeapi.co/api/v2/pokemon/";
+        for(int i = 1; i <= 5; i++){
+            new AsyncHTTPRequest<>(new Pokemon(), uri + i, (poke) ->{
+                String frontImgURI = poke.frontPic;
+                new AsyncImageRequest(frontImgURI, (b) ->{
+                    for(String pokemon : pokemons){
+                        if(pokemon.equals(poke.name)){
+                            PokemonListItem li = new PokemonListItem();
+                            li.b = b;
+                            li.p = poke;
+                            adapter.listItem.add(li);
+                            adapter.notifyDataSetChanged();
+                            break;
+                        }
+                    }
+                }).execute();
+            }).execute();
+        }
+
 
 
     }
